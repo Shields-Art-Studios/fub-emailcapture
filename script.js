933aa3299d6d9855e5def7e5077c3a6a997d8956
Jack Woods <jackrwoods@gmail.com>
2019-01-16 19:01:19 -0800

    Prevent sending requests with an empty email address.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
  * @Date:   2019-01-14T08:06:55-08:00
  * @Filename: script.js
  * @Last modified by:   Jack Woods
- * @Last modified time: 2019-01-16T18:57:53-08:00
+ * @Last modified time: 2019-01-16T19:00:49-08:00
  * @Copyright: 2018 Oregon State University
  */
 // This script listens for a button press on the subscribe button, and then registers the user's email in FollowUpBoss.
@@ -18,38 +18,39 @@ var leadTags = ['changeyourowndiaper']
 
 // Add button event listener
 let button = document.getElementById('subscribe').addEventListener('click', function(event) {
-  var data = JSON.stringify({
-    'source': 'Change Your Own Diaper',
-    'type': 'General Inquiry',
-    'message': document.getElementById('et_pb_signup_email').value + ' subscribed to the ChangeYourOwnDiaper.com email list!\n',
-    'description': 'Notification',
-    'person': {
-      'emails': [ { 'value': document.getElementById('et_pb_signup_email').value, 'type': 'home' } ],
-      'tags': leadTags,
-      'sourceUrl': 'https://changeyourowndiaper.com/freebies/',
-      'assignedTo' : 'Nick Krautter'
-    },
-    'campaign': {
+  if (document.getElementById('et_pb_signup_email').value.length > 0) {
+    var data = JSON.stringify({
       'source': 'Change Your Own Diaper',
-      'medium': 'referral'
-    }
-  })
+      'type': 'General Inquiry',
+      'message': document.getElementById('et_pb_signup_email').value + ' subscribed to the ChangeYourOwnDiaper.com email list!\n',
+      'description': 'Notification',
+      'person': {
+        'emails': [ { 'value': document.getElementById('et_pb_signup_email').value, 'type': 'home' } ],
+        'tags': leadTags,
+        'sourceUrl': 'https://changeyourowndiaper.com/freebies/',
+        'assignedTo' : 'Nick Krautter'
+      },
+      'campaign': {
+        'source': 'Change Your Own Diaper',
+        'medium': 'referral'
+      }
+    })
 
-  var xhr = new XMLHttpRequest()
-  xhr.addEventListener('readystatechange', function () {
-    if (this.readyState === this.DONE) {
-      document.getElementsByClassName('et_pb_newsletter_result et_pb_newsletter_success')[0].textContent = 'Success!'
-      console.log('Lead added with status: ' + this.status + ' and message: ')
-      console.log(JSON.parse(this.responseText))
+    var xhr = new XMLHttpRequest()
+    xhr.addEventListener('readystatechange', function () {
+      if (this.readyState === this.DONE) {
+        document.getElementsByClassName('et_pb_newsletter_result et_pb_newsletter_success')[0].textContent = 'Success!'
+        console.log('Lead added with status: ' + this.status + ' and message: ')
+        console.log(JSON.parse(this.responseText))
 
-      // Disable the button
-      button.disabled = true
-    }
-   })
+        // Disable the button
+        button.disabled = true
+      }
+     })
 
-  xhr.open('POST', 'https://api.followupboss.com/v1/events')
-  xhr.setRequestHeader('Authorization', 'Basic ' + btoa('483fb0459ff828db2f8962ac6053ca28f79c07:'))
-  xhr.setRequestHeader('Content-Type', 'application/json')
-  xhr.send(data)
-
+    xhr.open('POST', 'https://api.followupboss.com/v1/events')
+    xhr.setRequestHeader('Authorization', 'Basic ' + btoa('483fb0459ff828db2f8962ac6053ca28f79c07:'))
+    xhr.setRequestHeader('Content-Type', 'application/json')
+    xhr.send(data)
+  }
 })
