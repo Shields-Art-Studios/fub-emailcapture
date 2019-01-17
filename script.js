c832114dfd7db329659b09ac2f5a547363ae43d2
Jack Woods <jackrwoods@gmail.com>
2019-01-16 19:50:49 -0800

    Change results to result

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
  * @Date:   2019-01-14T08:06:55-08:00
  * @Filename: script.js
  * @Last modified by:   Jack Woods
- * @Last modified time: 2019-01-16T19:46:30-08:00
+ * @Last modified time: 2019-01-16T19:50:38-08:00
  * @Copyright: 2018 Oregon State University
  */
 // This script listens for a button press on the subscribe button, and then registers the user's email in FollowUpBoss.
@@ -48,7 +48,7 @@ let button = document.getElementById('subscribe').addEventListener('click', func
         clicked = 1
 
         // Show success message
-        document.getElementsByClassName('results')[0].textContent = 'Success'
+        document.getElementsByClassName('result')[0].textContent = 'Success'
       }
      })
 
